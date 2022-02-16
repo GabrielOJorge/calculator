2b0a8c21e749531e5e6b69ab5658d667877a38c8
GabrielOJorge <goliver331@protonmail.com>
2022-02-15 21:47:24 -0300

    js: add a event to each operator btn that attribute the element value as the operator variable value

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,13 @@ const percentageBtn = document.getElementById("percentage-btn");
 const clearBtn = document.getElementById("clear-btn");
 const divisionBtn = document.getElementById("division-btn");
 const multiplBtn = document.getElementById("multipl-btn");
-const substractBtn = document.getElementById("minus-btn");
+const substractBtn = document.getElementById("substract-btn");
 const plusBtn = document.getElementById("plus-btn");
 const posNegBtn = document.getElementById("pos-neg");
 const dotBtn = document.getElementById("dot-btn");
 const equalBtn = document.getElementById("equal-btn");
 const numBtns = document.querySelectorAll(".num");
+const operatorsBtns = document.querySelectorAll(".operator");
 
 let x = 0;
 let y = 0;
@@ -26,13 +27,13 @@ const getBtnValue = value => {
 };
 
 const operate = () => {
-  if (op === "+") {
+  if (operator === "+") {
     calcDisplay.textContent = add()
-  } else if (op === "*") {
+  } else if (operator === "*") {
     calcDisplay.textContent = multiply()
-  } else if (op === "-") {
+  } else if (operator === "-") {
     calcDisplay.textContent = substract()
-  } else if (op === "/") {
+  } else if (operator === "/") {
     calcDisplay.textContent = divide()
   }
 };
@@ -41,3 +42,6 @@ numBtns.forEach(btn => btn.addEventListener("click", () => {
   calcDisplay.textContent += `${btn.value}`;
   getBtnValue(btn.value);
 }));
+
+operatorsBtns.forEach(op => op.addEventListener("click", () => operator = op.value));
+equalBtn.addEventListener("click", operate);
